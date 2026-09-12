585b5896165d31df4998a55318c2a9b3299b3756
Isaac Eswa <isaaceswa@gmail.com>
2026-09-12 12:46:24 +0200

    #3 added HTML for the keycord section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,41 @@
 <script></script>
 
-<main></main>
+<main>
+  <section class="keycord">
+    <img src="src/lib/assets/images/keycord.png" alt="Keycord" />
+    <div class="card">
+      <h1>Squad member</h1>
+      <img
+        src="src/lib/assets/images/isaac.jpg"
+        alt="Foto van Isaac Eswarapatham"
+      />
+    </div>
+
+    <div class="personal-information">
+      <ul>
+        <li>
+          <div class="personal-information-field">
+            <h2>Name</h2>
+            <p>Isaac Eswarapatham</p>
+          </div>
+        </li>
+
+        <li>
+          <div class="personal-information-field">
+            <h2>Team</h2>
+            <p>CoffeeTime</p>
+          </div>
+        </li>
+
+        <li>
+          <div class="personal-information-field">
+            <h2>Song</h2>
+            <p>90210</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
 
 <style></style>
