eaed477e5c1976fb41cbd0e96ebcb2c59bc443b4
Isaac Eswa <isaaceswa@gmail.com>
2026-09-15 16:56:09 +0200

    #10 #7 added title element in the head and changed +page.server.js to +layout.server.js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script>
   import favicon from "$lib/assets/images/favicon.svg";
 
-  let { children } = $props();
+  let { children, data } = $props();
+  const person = data.person;
+
+  // let { children } = $props();
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
   <link rel="stylesheet" href="/src/styles/styleguide.css" />
+  <title>Profile card van {person.name}</title>
 </svelte:head>
 
 {@render children()}
