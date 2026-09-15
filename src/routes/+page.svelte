f17774a07d2f2294264eb279964d365da3372e97
Isaac Eswa <isaaceswa@gmail.com>
2026-09-15 10:06:58 +0200

    #6 installed adapter for Netfliy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main style="--bg-color: {form?.color ?? '#E5E2DD'}">
-  <!-- <section class="keycord">
+  <section class="keycord">
     <img src="src/lib/assets/images/keycord.png" alt="Keycord" />
     <div class="card">
       <h1>Squad member</h1>
@@ -38,20 +38,7 @@
         </li>
       </ul>
     </div>
-  </section> -->
-
-  <!-- <section class="background-color-selector">
-    <form>
-      <input type="color" list="colors" name="color" value="#E5E2DD" />
-      <datalist id="colors">
-        <option value="#E5E2DD"></option>
-        <option value="#F5693C"></option>
-        <option value="#722F37"></option>
-        <option value="#031C33"></option>
-        <option value="#302621"></option>
-      </datalist>
-    </form>
-  </section> -->
+  </section>
 
   <section class="social-links">
     <a href="https://github.com/IsaacEswa" target="_blank">Github</a>
